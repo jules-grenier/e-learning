6cbc8a334b0fcd2c845dc545d7cf8a4f0ecce758
Jules Grenier <jules-grenier@outlook.com>
2022-01-18 21:54:10 +0100

    build: project setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>Bienvenue sur la plateforme d'e-learning</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style lang="scss">
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/vue-agile.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+declare module "vue-agile" {
+  import Vue, { PluginObject } from "vue";
+
+  const VueAgile: PluginObject<{}>;
+  export default VueAgile;
+  export const Agile: Vue;
+}
