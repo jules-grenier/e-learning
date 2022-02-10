9c790a15804bd0466774303cbbf9bce7becd9bec
Jules Grenier <jules-grenier@outlook.com>
2022-02-10 10:20:34 +0100

    feat: better course overview page

--- src/views/CourseOverview.vue
+++ src/views/CourseOverview.vue
@@ -1,18 +1,31 @@
 <template>
   <LayoutTopSpace>
-    <h1>{{ course.title }}</h1>
-    <span>{{ course.author_name }}</span>
-    <p>{{ course.description }}</p>
+    <div class="grid">
+      <div class="shop-details">
+        <img :src="image" alt="course image" class="course-image" />
+        <span class="course-price">{{ course.price }}€</span>
 
-    <div class="actions">
-      <button v-if="!courseIsInCart" @click="addToCart">Ajouter au panier</button>
-      <button v-if="courseIsInCart" @click="removeFromCart">Enlever du panier</button>
-    </div>
+        <div class="cart-actions">
+          <button v-if="!courseIsInCart" @click="addToCart" class="cart-btn btn-add">Ajouter au panier</button>
+          <button v-if="courseIsInCart" @click="removeFromCart" class="cart-btn btn-remove">Enlever du panier</button>
+        </div>
+      </div>
+      <div class="course-details">
+        <div class="course-general-info">
+          <h1 class="course-title">{{ course.title }}</h1>
+          <span class="course-author">par {{ course.author_name }}</span>
+          <p class="course-description">{{ course.description }}</p>
+        </div>
 
-    <div class="content">
-      <h2>Contenu de la formation</h2>
-      <div class="file" v-for="file in course.content" :key="file.id">
-        <p>{{ file.description }}</p>
+        <div class="course-content">
+          <h2>Contenu de la formation</h2>
+          <div v-if="course.content" class="content-length">{{ course.content.length }} sessions</div>
+          <div class="content-items">
+            <div class="content-item" v-for="(file, index) in course.content" :key="file.id">
+              <p>{{ index + 1 }}. {{ file.description }}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </LayoutTopSpace>
@@ -20,6 +33,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import axios from "axios";
 
 import { LayoutTopSpace } from "@/layouts";
 
@@ -29,6 +43,7 @@ export default defineComponent({
     return {
       course: {},
       courseIsInCart: false,
+      image: "https://via.placeholder.com/350x200",
     };
   },
   methods: {
@@ -43,11 +58,111 @@ export default defineComponent({
   },
   mounted() {
     this.$nextTick(async () => {
-      this.course = await this.$store.dispatch("courses/getCourse", this.$route.params.id);
+      axios.get(`http://localhost:8090/courses/${this.$route.params.id}`).then((res) => {
+        this.course = res.data;
+      });
       this.courseIsInCart = await this.$store.dispatch("cart/isInCart", this.course);
     });
   },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.grid {
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+}
+
+.shop-details {
+  border: 1px solid var(--color-grey-2);
+  border-radius: var(--border-radius);
+  background-color: var(--color-white);
+
+  .course-image {
+    max-width: 350px;
+    border-radius: var(--border-radius);
+  }
+
+  .course-price {
+    font-weight: bold;
+    font-size: 24px;
+    display: block;
+    margin-top: 20px;
+    padding: 0px 40px;
+  }
+
+  .cart-actions {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    padding: 0px 40px;
+
+    .cart-btn {
+      padding: 15px 30px;
+      border: 1px solid transparent;
+      font-size: 16px;
+      border-radius: var(--border-radius);
+      width: 100%;
+      cursor: pointer;
+
+      &.btn-add {
+        background-color: var(--color-blue);
+        border-color: var(--color-blue);
+        color: var(--color-white);
+
+        &:hover {
+          background-color: var(--color-black);
+          border-color: var(--color-black);
+        }
+      }
+
+      &.btn-remove {
+        background-color: transparent;
+        border-color: var(--color-black);
+        color: var(--color-black);
+
+        &:hover {
+          background-color: var(--color-black);
+          color: var(--color-white);
+        }
+      }
+    }
+  }
+}
+
+.course-details {
+  flex: 1;
+
+  .course-general-info {
+    .course-author {
+      margin-top: 10px;
+    }
+
+    .course-description {
+      margin-top: 20px;
+    }
+  }
+
+  .course-content {
+    margin-top: 40px;
+
+    .content-length {
+      margin-top: 20px;
+    }
+
+    .content-items {
+      margin-top: 10px;
+      padding: 20px;
+      border: 1px solid var(--color-grey-2);
+      border-radius: var(--border-radius);
+      background-color: var(--color-white);
+
+      .content-item {
+        & + .content-item {
+          margin-top: 15px;
+        }
+      }
+    }
+  }
+}
+</style>
