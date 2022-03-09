76222e44ab4691037c541ca0b46ad8ffa5bac68f
Jules Grenier <jules-grenier@outlook.com>
2022-03-09 15:25:22 +0100

    feat: list user courses

--- src/views/MyCourses.vue
+++ src/views/MyCourses.vue
@@ -1,17 +1,40 @@
 <template>
   <LayoutTopSpace>
-    <h1>Mes formations</h1>
+    <h1>Mon apprentissage</h1>
+
+    <section class="courses">
+      <course-owned v-for="course in courses" :key="course.id" :course="course" size="small" />
+    </section>
   </LayoutTopSpace>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, VuexState } from "vue";
+import { mapState } from "vuex";
 
 import { LayoutTopSpace } from "@/layouts";
+import CourseOwned from "@/components/shared/Course/CourseOwned.vue";
 
 export default defineComponent({
-  components: { LayoutTopSpace },
+  components: { LayoutTopSpace, CourseOwned },
+  computed: {
+    ...mapState({
+      courses: (state) => (state as VuexState).user.courses,
+    }),
+  },
+  async mounted() {
+    const rawUser = localStorage.getItem("user");
+
+    if (rawUser) {
+      const token = JSON.parse(rawUser).token;
+      await this.$store.dispatch("user/fetchCourses", token);
+    }
+  },
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+section {
+  margin-top: 80px;
+}
+</style>
